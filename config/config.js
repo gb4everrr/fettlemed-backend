228d0c84780a1fc915aff5b07a30bd46a57de3a1
Gurubalaji <gbofficial.060@gmail.com>
2025-09-25 00:07:08 +0530

    Production DB Configuration for Render

--- config/config.js
+++ config/config.js
@@ -9,4 +9,17 @@ module.exports = {
     port: process.env.DB_PORT,
     dialect: 'postgres',
   },
+  production: {
+    // This tells Sequelize to use the DATABASE_URL environment variable
+    // we set up in the Render dashboard.
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      // This is required for connecting to Neon
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // This may be needed to avoid certificate errors
+      },
+    },
+  },
 };
