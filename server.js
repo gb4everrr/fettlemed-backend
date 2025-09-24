1c6e0c62f6d4b8137f051092585d18eeb2022804
Gurubalaji <gbofficial.060@gmail.com>
2025-09-25 02:33:08 +0530

    feat: Port fixes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const app = require('./app');
 const { sequelize } = require('./models');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 sequelize.sync().then(() => {
   console.log('Database connected');
